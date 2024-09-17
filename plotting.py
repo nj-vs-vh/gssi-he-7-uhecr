e832ed43f59bea7c29d47a22d4d781677543097b
Igor Vaiman <igor.vaiman@inten.to>
2024-09-17 16:22:19 +0200

    init

---
+++ plotting.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+from astropy.visualization import quantity_support  # type: ignore
+
+quantity_support()
+
+
+def label_axes(ax: plt.Axes, xlabel: str | None = None, ylabel: str | None = None):
+    for label, getter, setter in (
+        (xlabel, ax.get_xlabel, ax.set_xlabel),
+        (ylabel, ax.get_ylabel, ax.set_ylabel),
+    ):
+        if label is None:
+            continue
+        units_label = getter().strip()
+        if not units_label or units_label == "$\\mathrm{}$":
+            units = ""
+        else:
+            units = f" [{units_label}]"
+        setter(f"$ {label} $" + units)
